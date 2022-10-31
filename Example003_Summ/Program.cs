d528ea3bf992b1e9d6e7933669fefdae9ca2cb61
Karina Undalova <undalova2012@mail.ru>
2022-10-31 22:18:20 +0300

    Example003RandomSum

--- Example003_Summ/Program.cs
+++ Example003_Summ/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 31;
-int namberB = 5;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int namberB = new Random().Next(1, 10);
+Console.WriteLine(namberB);
 int result = numberA + namberB;
 Console.WriteLine(result);
 
